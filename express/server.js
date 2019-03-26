8aa5af2d89f65470bd388981720a782fe0e4b3d8
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2019-03-26 16:38:01 +0100

    test static

--- express/server.js
+++ express/server.js
@@ -13,13 +13,13 @@ app.use(basicAuth({
   users: { 'admin': 'supersecret' }
 }));
 
-// router.get('*', (req, res) => {
-//   res.sendFile("index.html", { root: __dirname+"/../build/" });
-// });
+router.get('*', (req, res) => {
+  res.sendFile("index.html", { root: __dirname+"/../build/" });
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(express.static(__dirname + "/../build"));
+// app.use(express.static(__dirname + "/../build"));
 app.use('/',router);
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
