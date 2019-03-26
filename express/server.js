165ca8f462df300c6148c54ea2a651a75040e841
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2019-03-26 16:45:33 +0100

    test static 3

--- express/server.js
+++ express/server.js
@@ -14,7 +14,9 @@ app.use(basicAuth({
 }));
 
 router.get('*', (req, res) => {
-  res.sendFile("index.html", { root: __dirname+"/.." });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
