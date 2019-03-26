167f90dc83c306590e4a3882d4f7d1734c3d46a9
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2019-03-26 16:15:52 +0100

    test

--- express/server.js
+++ express/server.js
@@ -3,16 +3,23 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const basicAuth = require('express-basic-auth')
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+console.log("Started");
+
+app.use(basicAuth({
+  challenge: true,
+  users: { 'admin': 'supersecret' }
+}));
+
+router.get('*', (req, res) => {
+  res.sendFile("index.html", { root: __dirname+"/.." });
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+app.use('/api',router);
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
