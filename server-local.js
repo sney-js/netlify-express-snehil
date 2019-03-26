4c7eb00be13938895f197fcdf9d4e675e08d53d1
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2019-03-26 16:36:25 +0100

    test static

--- server-local.js
+++ server-local.js
@@ -1,11 +1,4 @@
 'use strict';
 
 const app = require('./express/server');
-// app.listen(3000, () => console.log('Local app listening on port 3000!'));
-
-// var express = require('express')
-// var app = express()
-// app.get('/', function (req, res) {
-//   res.send('hello world')
-// })
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
